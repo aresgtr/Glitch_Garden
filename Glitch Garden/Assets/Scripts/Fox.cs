1fca4959e2f24daaea4ca73c1df8709a87d976e6
Qi <784359442@qq.com>
2020-05-14 17:35:13 -0700

    Add attacker fox

---
+++ Glitch Garden/Assets/Scripts/Fox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        
+        //    If this is a gravestone then do stuff...
+        if (otherObject.GetComponent<Gravestone>())
+        {
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }
+
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}
